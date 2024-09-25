0cb90ffe20eefb25a32c0ea7608b9f4288354bcd
kurlee710 <josephatamaniuk@gmail.com>
2024-09-24 21:19:06 -0400

    created constructor for truck class

--- Develop/src/classes/Truck.ts
+++ Develop/src/classes/Truck.ts
@@ -25,6 +25,34 @@ class Truck extends Vehicle implements AbleToTow {
   // TODO: The constructor should call the constructor of the parent class, Vehicle
   // TODO: The constructor should initialize the properties of the Truck class
   // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
+  constructor(
+    vin: string,
+    make: string,
+    model: string,
+    color: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[],
+    towingCapacity: number
+  ) {
+    super();
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    this.wheels = wheels;
+    this.towingCapacity = towingCapacity;
+
+    if (wheels.length !== 4) {
+      this.wheels = [new Wheel(), new Wheel(), new Wheel(), new Wheel()];
+    } else {
+      this.wheels = wheels;
+    }
+  }
 
   // TODO: Implement the tow method from the AbleToTow interface
   tow(vehicle: Truck | Motorbike | Car): void {
