d813fcc90699fe231bd5d160f6c4bebe8035e146
kurlee710 <josephatamaniuk@gmail.com>
2024-09-24 21:47:20 -0400

    added tow method

--- Develop/src/classes/Cli.ts
+++ Develop/src/classes/Cli.ts
@@ -283,7 +283,7 @@ class Cli {
 
   // method to find a vehicle to tow
   // TODO: add a parameter to accept a truck object
-  findVehicleToTow(): void {
+  findVehicleToTow(truck: Truck): void {
     inquirer
       .prompt([
         {
@@ -299,6 +299,14 @@ class Cli {
         },
       ])
       .then((answers) => {
+        const vehicleToTow = answers.vehicleToTow;
+        if (vehicleToTow === truck) {
+          console.log("A truck cannot tow itself!");
+          this.performActions();
+        } else {
+          truck.tow(vehicleToTow);
+          this.performActions();
+        }
         // TODO: check if the selected vehicle is the truck
         // TODO: if it is, log that the truck cannot tow itself then perform actions on the truck to allow the user to select another action
         // TODO: if it is not, tow the selected vehicle then perform actions on the truck to allow the user to select another action
